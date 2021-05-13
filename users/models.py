47b7b105b7dce446956274ef2d748b53bb91e456
Shahriyar Rzayev <rzayev.sehriyar@gmail.com>
2021-05-13 23:22:40 +0400

    Code changes for episode-3

---
+++ users/models.py
@@ -0,0 +1,8 @@
+from app.main import db
+
+
+class User(db.Model):
+    __tablename__ = "users"
+
+    id = db.Column(db.BigInteger(), primary_key=True)
+    nickname = db.Column(db.Unicode(), default="unnamed")
